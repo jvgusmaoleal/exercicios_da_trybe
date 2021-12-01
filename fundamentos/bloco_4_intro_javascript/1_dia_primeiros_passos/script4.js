bc9d829e963b192dd489fe19ae62fed684716f62
joao_victor_leal <joaovictorgusmaoleal@hotmail.com>
2021-12-01 15:47:18 -0300

    parte inicial dos exercicios

--- fundamentos/bloco_4_intro_javascript/1_dia_primeiros_passos/script4.js
+++ fundamentos/bloco_4_intro_javascript/1_dia_primeiros_passos/script4.js
@@ -8,10 +8,10 @@ let salarioLiquido;
 if(salarioBruto <= 1556,94){
     inss= (salarioBruto * 0,08);
 }
-    if(salarioBruto > 1556,95 && salarioBruto<= 2594,92){
+    else if(salarioBruto > 1556,95 && salarioBruto<= 2594,92){
         inss= (salarioBruto * 0,09);
         }
-        if(salarioBruto > 2594,92&& salarioBruto<= 5189,82){
+       else if(salarioBruto > 2594,92&& salarioBruto<= 5189,82){
             inss= (salarioBruto * 0,11);
             }
             else{
@@ -22,13 +22,13 @@ if(salarioBruto <= 1556,94){
 if(salarioBruto <= 1903,98){
     ir= 0;
 }
-if(salarioBruto> 1903,98 && salarioBruto<= 2826,65){
-    ir= (salarioBruto*0,075) +142,80;
-    }
-    if(salarioBruto> 2826,65 && salarioBruto<= 3751,05){
+    else if(salarioBruto> 1903,98 && salarioBruto<= 2826,65){
+        ir= (salarioBruto*0,075) +142,80;
+         }
+        else if(salarioBruto> 2826,65 && salarioBruto<= 3751,05){
         ir= (salarioBruto*0,15) +354,80;
         }
-        if(salarioBruto> 3751,05 && salarioBruto<= 4664,68){
+        else if(salarioBruto> 3751,05 && salarioBruto<= 4664,68){
             ir= (salarioBruto*0,225) +636,13;
             }
             else{
